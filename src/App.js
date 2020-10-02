db5418a6ce623f48dc7cd5a82a33bb0dc982158f
Erick Sutil <ericksutil@gmail.com>
2020-10-02 14:06:25 -0300

    Request for Spotify using external aplication

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const url_end = 'https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=medium_term';
+
+class App extends React.Component {
+  constructor(props){
+    super();
+    this.parametros = this.getHashParams();
+    this.token = this.parametros.access_token;
+  }
+
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
+    }
+    return hashParams;
+  }
+
+  componentDidMount(){
+    fetch(url_end, {
+      method: 'get',
+      headers: {
+        Authorization: `Bearer ${this.token}`
+      }
+    })
+    .then(response => response.json())
+  }
 }
 
 export default App;
