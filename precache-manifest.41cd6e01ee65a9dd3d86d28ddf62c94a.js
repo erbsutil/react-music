37be5bbef8bb416a75e8fcb00b789ff36dfdd2b2
Erick Sutil <ericksutil@gmail.com>
2020-10-04 20:02:06 -0300

    Updates

---
+++ precache-manifest.41cd6e01ee65a9dd3d86d28ddf62c94a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6029a7cd7f2614a24b42d2f8f32a52ed",
+    "url": "/react-music/index.html"
+  },
+  {
+    "revision": "235d6fa6901c7cf9c9d0",
+    "url": "/react-music/static/css/main.17534587.chunk.css"
+  },
+  {
+    "revision": "50da91ecbbf246ec8240",
+    "url": "/react-music/static/js/2.50e2f289.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-music/static/js/2.50e2f289.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "235d6fa6901c7cf9c9d0",
+    "url": "/react-music/static/js/main.497bc9ae.chunk.js"
+  },
+  {
+    "revision": "570254a9e937af59b060",
+    "url": "/react-music/static/js/runtime-main.705d3365.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e461c81b7c1c8740dfa22f8dabd7780f.js"
+  "/react-music/precache-manifest.41cd6e01ee65a9dd3d86d28ddf62c94a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-music/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
