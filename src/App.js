e4cc9d2e7c4cc523ae13b6b45c875ea4365dc1a6
Erick Sutil <ericksutil@gmail.com>
2020-10-05 10:03:39 -0300

    Removing unused style class

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ class App extends Component {
         {!this.state.token && (
           <div>
             <a
-              className="btn btn--loginApp-link"
+              className="btn"
               href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
             >
               Login com Spotify
